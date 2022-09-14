2ecf410c3b7cb305f62c7a74b0dfb9ecf9b103ab
Pattarakorn Kuncharoen <pattarakorn.kunc@kmutt.ac.th>
2022-09-14 20:34:55 +0700

    Test1

--- totalPages.js
+++ totalPages.js
@@ -2,6 +2,13 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  if (arrayItems === undefined || arrayItems === null) {
+    return undefined
+  } else if (rowsPerPage === undefined || rowsPerPage === null || rowsPerPage == 0) {
+    return 1
+  } else {
+    return Math.ceil(arrayItems.length / rowsPerPage)
+  }
 }
 //12121312121
 
